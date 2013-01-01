a06afb20ce76520d122d6416ab07e0112d68b6ec
Daniel Mecke <daniel.mecke@cunningsoft.de>
2013-01-01 11:03:51 +0100

    initial commit

---
+++ src/Cunningsoft/Copperegg/Copperegg.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Cunningsoft\Copperegg;
+
+use Buzz\Browser;
+use Buzz\Client\Curl;
+use Buzz\Message\Request;
+use Buzz\Message\MessageInterface;
+use \DateTime;
+
+class Copperegg
+{
+    private $apiKey;
+
+    public function __construct($apiKey)
+    {
+        $this->apiKey = $apiKey;
+    }
+
+    /**
+     * @param \DateTime $startDate
+     * @param \DateTime $endDate
+     * @param string $note
+     * @param array $tags
+     *
+     * @return MessageInterface
+     */
+    public function createAnnotation(DateTime $startDate, DateTime $endDate, $note, $tags = array())
+    {
+        $request = new Request();
+        $request->setMethod('post');
+        $request->setProtocolVersion(1.1);
+        $request->addHeader('Authorization: Basic ' . base64_encode($this->apiKey . ':U'));
+        $request->setHost('https://api.copperegg.com');
+        $request->setResource('/v2/annotations.json');
+        $request->setContent('note=' . $note . '&starttime=' . $startDate->getTimestamp() . '&endtime=' . $endDate->getTimestamp() . '&tag=' . implode(',', $tags));
+
+        $browser = new Browser(new Curl());
+
+        return $browser->send($request);
+    }
+}
